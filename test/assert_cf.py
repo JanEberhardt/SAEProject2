6bda863cb02abe4cc74357993b65dc64dd6fbf83
JanEberhardt <ebejan93@gmail.com>
2015-05-20 15:48:52 +0200

    TODO: bool_cast.py

---
+++ test/assert_cf.py
@@ -0,0 +1,9 @@
+def main(x):
+    assert(False)
+    if (x>10):
+        return 1;
+    else:
+        return 2
+
+def expected_result():
+    return [1,2]
---
+++ test/bool_cast.py
@@ -0,0 +1,8 @@
+def main(x,y):
+    if (x<y) + x == 5:
+        return 0
+    else:
+        return 1
+
+def expected_result():
+    return [0,1]
